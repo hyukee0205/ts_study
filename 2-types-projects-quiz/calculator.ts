c3a4358c3f8accd2dbb7cfb4a535005d7db6706e
전찬혁 <hyukee0205@gmail.com>
2023-04-02 23:58:38 +0900

    0402

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,6 +1,25 @@
 /**
  * Let's make a calculator 🧮
  */
+
+type Command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder';
+function calculate(command: Command, b:number, c:number):number {
+  switch (command) {
+    case 'add':
+      return b+c;
+    case 'substract':
+      return b-c;
+    case 'multiply':
+      return b*c;
+    case 'divide':
+      return b/c;
+    case 'remainder':
+      return b%c;
+    default:
+      throw new Error('unknown command');
+  }
+}
+
 console.log(calculate('add', 1, 3)); // 4
 console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
