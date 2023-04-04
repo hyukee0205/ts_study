06539d63957ee4df7bb9192d90bae81b869b11de
전찬혁 <hyukee0205@gmail.com>
2023-04-04 23:57:51 +0900

    0404

---
+++ 3-oop2/3-3-encapsulation.ts
@@ -0,0 +1,49 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  // public
+  // private
+  // protected
+
+  class CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0; // instance (object) level
+  
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number):CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if(beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+      this.coffeeBeans += beans;
+    }
+
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+      return {
+        shots: shots,
+        hasMilk: false,
+      };
+    };
+  }
+
+
+  
+  const maker = CoffeeMaker.makeMachine(32);
+  maker.fillCoffeeBeans(32);  
+
+
+}
