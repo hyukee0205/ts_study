b6684d353811c42648227d350c2b3fac02293eb0
전찬혁 <hyukee0205@gmail.com>
2023-04-05 03:12:07 +0900

    0405

--- 3-oop2/3-3-encapsulation.ts
+++ 3-oop2/3-3-encapsulation.ts
@@ -46,4 +46,23 @@
   maker.fillCoffeeBeans(32);  
 
 
+  class User {
+    private firstName: string;
+    private lastName: string;
+    get fullName():string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+
+
+    constructor(firstName: string, lastName:string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  }
+
+  const user = new User('Steve', 'Jobs');
+  console.log(user.fullName);
+  user.firstName = 'Ellie';
+  console.log(user.fullName);
+  
 }
